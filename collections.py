a49651ec1667cb56a6652b1c7f3b0d23896f8c85
stom <tomsteventon@gmail.com>
2024-02-20 17:26:14 +0000

    fix: avoid crash when dldeting collection

--- collections.py
+++ collections.py
@@ -4,16 +4,24 @@ import bpy
 def DeleteCollection(
 	col           : bpy.types.Collection,
 	remove_objects: bool = False,
-):
+) -> bool:
 	"""
 	Deletes a collection, optionally removing all objects within it
 	"""
+
 	# Delete all objects in the scene
 	if remove_objects:
-		for obj in col.all_objects:   
-			#bpy.data.objects.unlink(obj)
+		# Make a list for all objects in the collection
+		# If we try and loop through the collection and delete at the same time, Blender may crash
+		objects = []
+		for obj in col.all_objects:
+			obj.select_set(False)
+			objects.append(obj)
+
+		for obj in objects:   
 			bpy.data.objects.remove(obj)
 	
 	# Delete the collection itself
 	bpy.data.collections.remove(col)
 	
+	return True
